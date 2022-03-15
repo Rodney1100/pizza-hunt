a9bfb9014ee005e228cded60dbb191a35755da3f
Rodney P <rodney.plancher@gmail.com>
2022-03-15 16:07:57 -0400

    heroku test 10

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/pizza-hunt", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1/pizza-hunt", {
   useFindAndModify: false,
   useNewUrlParser: true,
   useUnifiedTopology: true,
