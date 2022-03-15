0ff779a998b5611fd29c5500b20c13d5f02d3956
Rodney P <rodney.plancher@gmail.com>
2022-03-15 04:06:05 -0400

    heroku test 2

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -12,6 +12,7 @@ router.route("/:pizzaId").post(addComment);
 // /api/comments/<pizzaId>/<commentId>
 router.route("/:pizzaId/:commentId").put(addReply).delete(removeComment);
 
+// /api/comments/<pizzaId>/<commentId>/<replyId>
 router.route("/:pizzaId/:commentId/:replyId").delete(removeReply);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -7,13 +8,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.use(require("./routes"));
-mongoose.connect(process.env.MONGOOSE_URI || "mongodb://127.0.0.1/pizza-hunt", {
-  // useFindAnyModify: false,
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/pizza-hunt", {
+  useFindAndModify: false,
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-// use this to log mongo queries being executed!
+// Use this to log mongo queries being executed!
 mongoose.set("debug", true);
+
+app.use(require("./routes"));
+
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
