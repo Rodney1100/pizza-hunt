2203ec9d4bd33ffacd4217f0e2548c2f40f73e74
Rodney P <rodney.plancher@gmail.com>
2022-03-15 15:20:47 -0400

    heroku test 4

--- routes/api/pizza-routes.js
+++ routes/api/pizza-routes.js
@@ -6,8 +6,11 @@ const {
   updatePizza,
   deletePizza,
 } = require("../../controllers/pizza-controller");
-// Set up GET all and POST at /api/pizzas
+
+// /api/pizzas
 router.route("/").get(getAllPizza).post(createPizza);
-// Set up GET one, PUT, and DELETE at /api/pizzas/:id
+
+// /api/pizzas/:id
 router.route("/:id").get(getPizzaById).put(updatePizza).delete(deletePizza);
+
 module.exports = router;
